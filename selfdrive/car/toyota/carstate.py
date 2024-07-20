cd0ccd201d605e3171968dab135928945935bd99
Andrew Buck <anbuck@gmail.com>
2024-07-20 14:17:53 -0700

    Squashed commit of anbuck/rav4prime

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -31,10 +31,15 @@ class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
     can_define = CANDefine(DBC[CP.carFingerprint]["pt"])
-    self.shifter_values = can_define.dv["GEAR_PACKET"]["GEAR"]
     self.eps_torque_scale = EPS_SCALE[CP.carFingerprint] / 100.
     self.cluster_speed_hyst_gap = CV.KPH_TO_MS / 2.
     self.cluster_min_speed = CV.KPH_TO_MS / 2.
+
+    if CP.flags & ToyotaFlags.GEAR_PACKET_HYBRID.value:
+      self.shifter_values = can_define.dv["GEAR_PACKET_HYBRID"]["GEAR"]
+    else:
+      self.shifter_values = can_define.dv["GEAR_PACKET"]["GEAR"]
+
 
     # On cars with cp.vl["STEER_TORQUE_SENSOR"]["STEER_ANGLE"]
     # the signal is zeroed to where the steering angle is at start.
@@ -46,6 +51,7 @@ class CarState(CarStateBase):
     self.distance_button = 0
 
     self.pcm_follow_distance = 0
+    self.pcm_acc_status = 0
 
     self.low_speed_lockout = False
     self.acc_type = 1
@@ -92,7 +98,11 @@ class CarState(CarStateBase):
       ret.gasPressed = ret.gas > 805
     else:
       # TODO: find a common gas pedal percentage signal
-      ret.gasPressed = cp.vl["PCM_CRUISE"]["GAS_RELEASED"] == 0
+      if self.CP.flags & ToyotaFlags.ALT_GAS_MSG.value:
+        ret.gas = cp.vl["GAS_PEDAL"]["GAS_PEDAL_USER"]
+        ret.gasPressed = cp.vl["GAS_PEDAL"]["GAS_PEDAL_USER"] > 0
+      else:
+        ret.gasPressed = cp.vl["PCM_CRUISE"]["GAS_RELEASED"] == 0
 
     ret.wheelSpeeds = self.get_wheel_speeds(
       cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"],
@@ -123,12 +133,15 @@ class CarState(CarStateBase):
         ret.steeringAngleOffsetDeg = self.angle_offset.x
         ret.steeringAngleDeg = torque_sensor_angle_deg - self.angle_offset.x
 
-    can_gear = int(cp.vl["GEAR_PACKET"]["GEAR"])
+    if self.CP.flags & ToyotaFlags.GEAR_PACKET_HYBRID.value:
+      can_gear = int(cp.vl["GEAR_PACKET_HYBRID"]["GEAR"])
+    else:
+      can_gear = int(cp.vl["GEAR_PACKET"]["GEAR"])
     ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(can_gear, None))
     ret.leftBlinker = cp.vl["BLINKERS_STATE"]["TURN_SIGNALS"] == 1
     ret.rightBlinker = cp.vl["BLINKERS_STATE"]["TURN_SIGNALS"] == 2
 
-    if self.CP.carFingerprint != CAR.TOYOTA_MIRAI:
+    if self.CP.carFingerprint not in [CAR.TOYOTA_MIRAI, CAR.TOYOTA_RAV4_PRIME]:
       ret.engineRpm = cp.vl["ENGINE_RPM"]["RPM"]
 
     ret.steeringTorque = cp.vl["STEER_TORQUE_SENSOR"]["STEER_TORQUE_DRIVER"]
@@ -181,12 +194,12 @@ class CarState(CarStateBase):
       # ignore standstill state in certain vehicles, since pcm allows to restart with just an acceleration request
       ret.cruiseState.standstill = self.pcm_acc_status == 7
     ret.cruiseState.enabled = bool(cp.vl["PCM_CRUISE"]["CRUISE_ACTIVE"])
-    ret.cruiseState.nonAdaptive = self.pcm_acc_status in (1, 2, 3, 4, 5, 6)
+    ret.cruiseState.nonAdaptive = cp.vl["PCM_CRUISE"]["CRUISE_STATE"] in (1, 2, 3, 4, 5, 6)
 
     ret.genericToggle = bool(cp.vl["LIGHT_STALK"]["AUTO_HIGH_BEAM"])
     ret.espDisabled = cp.vl["ESP_CONTROL"]["TC_DISABLED"] != 0
 
-    if not self.CP.enableDsu and not self.CP.flags & ToyotaFlags.DISABLE_RADAR.value:
+    if not self.CP.enableDsu and not self.CP.flags & ToyotaFlags.DISABLE_RADAR.value and not self.CP.flags & ToyotaFlags.SECOC.value:
       ret.stockAeb = bool(cp_acc.vl["PRE_COLLISION"]["PRECOLLISION_ACTIVE"] and cp_acc.vl["PRE_COLLISION"]["FORCE"] < -1e-5)
 
     if self.CP.enableBsm:
@@ -207,6 +220,9 @@ class CarState(CarStateBase):
       else:
         self.distance_button = cp.vl["SDSU"]["FD_BUTTON"]
 
+    if self.CP.flags & ToyotaFlags.SECOC.value:
+      self.secoc_synchronization = copy.copy(cp.vl["SECOC_SYNCHRONIZATION"])
+
     # FrogPilot CarState functions
     fp_ret.brakeLights = bool(cp.vl["ESP_CONTROL"]["BRAKE_LIGHTS_ACC"])
 
@@ -217,16 +233,28 @@ class CarState(CarStateBase):
 
     fp_ret.dashboardSpeedLimit = self.calculate_speed_limit(cp_cam, frogpilot_toggles)
 
-    fp_ret.ecoGear = cp.vl["GEAR_PACKET"]['ECON_ON'] == 1
-    fp_ret.sportGear = cp.vl["GEAR_PACKET"]['SPORT_ON_2' if self.CP.flags & ToyotaFlags.NO_DSU else 'SPORT_ON'] == 1
+    if self.CP.flags & ToyotaFlags.GEAR_PACKET_HYBRID.value:
+      # These mode values don't seem to exist for the GEAR_PACKET_HYBRID
+      fp_ret.ecoGear = False
+      fp_ret.sportGear = False
+    else:
+      fp_ret.ecoGear = cp.vl["GEAR_PACKET"]['ECON_ON'] == 1
+      fp_ret.sportGear = cp.vl["GEAR_PACKET"]['SPORT_ON_2' if self.CP.flags & ToyotaFlags.NO_DSU else 'SPORT_ON'] == 1
 
     if self.CP.carFingerprint != CAR.TOYOTA_PRIUS_V:
       self.lkas_previously_enabled = self.lkas_enabled
       message_keys = ["LDA_ON_MESSAGE", "SET_ME_X02"]
       self.lkas_enabled = any(self.lkas_hud.get(key) == 1 for key in message_keys)
 
-    self.pcm_accel_net = cp.vl["PCM_CRUISE"]["ACCEL_NET"]
-    self.pcm_neutral_force = cp.vl["PCM_CRUISE"]["NEUTRAL_FORCE"]
+    if "ACCEL_NET" in cp.vl["PCM_CRUISE"]:
+      self.pcm_accel_net = cp.vl["PCM_CRUISE"]["ACCEL_NET"]
+    else:
+      self.pcm_accel_net = 0
+
+    if "NEUTRAL_FORCE" in cp.vl["PCM_CRUISE"]:
+      self.pcm_neutral_force = cp.vl["PCM_CRUISE"]["NEUTRAL_FORCE"]
+    else:
+      self.pcm_neutral_force = 0
 
     # ZSS Support - Credit goes to the DragonPilot team!
     if self.CP.flags & ToyotaFlags.ZSS and self.zss_threshold_count < ZSS_THRESHOLD_COUNT:
@@ -258,7 +286,6 @@ class CarState(CarStateBase):
   @staticmethod
   def get_can_parser(CP):
     messages = [
-      ("GEAR_PACKET", 1),
       ("LIGHT_STALK", 1),
       ("BLINKERS_STATE", 0.15),
       ("BODY_CONTROL_STATE", 3),
@@ -273,7 +300,15 @@ class CarState(CarStateBase):
       ("STEER_TORQUE_SENSOR", 50),
     ]
 
-    if CP.carFingerprint != CAR.TOYOTA_MIRAI:
+    if CP.flags & ToyotaFlags.GEAR_PACKET_HYBRID.value:
+      messages.append(("GEAR_PACKET_HYBRID", 60))
+    else:
+      messages.append(("GEAR_PACKET", 1))
+
+    if CP.flags & ToyotaFlags.ALT_GAS_MSG.value:
+      messages.append(("GAS_PEDAL", 42))
+
+    if CP.carFingerprint not in [CAR.TOYOTA_MIRAI, CAR.TOYOTA_RAV4_PRIME]:
       messages.append(("ENGINE_RPM", 42))
 
     if CP.carFingerprint in UNSUPPORTED_DSU_CAR:
@@ -308,7 +343,13 @@ class CarState(CarStateBase):
         ("SDSU", 100),
       ]
 
-    messages += [("SECONDARY_STEER_ANGLE", 0)]
+    if CP.flags & ToyotaFlags.ZSS:
+      messages += [("SECONDARY_STEER_ANGLE", 0)]
+
+    if CP.flags & ToyotaFlags.SECOC.value:
+      messages += [
+        ("SECOC_SYNCHRONIZATION", 10),
+      ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], messages, 0)
 
@@ -328,9 +369,14 @@ class CarState(CarStateBase):
 
     if CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
       messages += [
-        ("PRE_COLLISION", 33),
         ("ACC_CONTROL", 33),
         ("PCS_HUD", 1),
+      ]
+
+    # TODO: Figure out new layout of the PRE_COLLISION message
+    if not CP.flags & ToyotaFlags.SECOC.value:
+      messages += [
+        ("PRE_COLLISION", 33),
       ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], messages, 2)
